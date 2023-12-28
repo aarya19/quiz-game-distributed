e15ece6cc3cc69c9532b5ae76bedad0da716bef6
Rahul Raj <118106534+rajanrahulraj@users.noreply.github.com>
2023-12-28 12:26:48 +0000

    Quiz response update events

--- gameService/src/main/java/com/game/gameService/controllers/GameController.java
+++ gameService/src/main/java/com/game/gameService/controllers/GameController.java
@@ -1,16 +1,21 @@
 package com.game.gameService.controllers;
 
 import com.game.common.KafkaProducerConfig;
+import com.game.core.Constants;
+import com.game.entities.GameEvent;
 import com.game.entities.Question;
 import com.game.entities.Quiz;
 import com.game.gameService.service.GameService;
 import com.game.utilities.MongoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import static com.game.core.Constants.QUIZ_EVENTS;
 
 
 @RestController
@@ -36,5 +41,25 @@ public class GameController {
         service.startGame(quiz);
 
         return "Started Game";
+    }
+
+    // Use the kafka
+    @KafkaListener(topics = QUIZ_EVENTS)
+    public void startGame(GameEvent event){
+        if (event.getEventType().equals(Constants.EVENT_TYPE.START_QUIZ.event())){
+            Quiz quiz = mongoService.fetchQuiz(event.getQuizId());
+            GameService service = new GameService(this.producer);
+            service.startGame(quiz);
+        }
+    }
+
+    @KafkaListener(topics = QUIZ_EVENTS)
+    public void updateResponse(GameEvent event){
+        if (event.getEventType().equals(Constants.EVENT_TYPE.UPDATE_RESPONSE.event())){
+
+            GameService service = new GameService(this.producer);
+
+            service.updateResponse(event);
+        }
     }
 }
