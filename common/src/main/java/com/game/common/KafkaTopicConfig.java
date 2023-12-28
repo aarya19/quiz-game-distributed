bd3d73fca9e2318ec7f2feabd40ea279645e29e8
Tehsein Firoze <73216889+tfiroze@users.noreply.github.com>
2023-12-28 18:38:46 +0000

    fix

--- common/src/main/java/com/game/common/KafkaTopicConfig.java
+++ common/src/main/java/com/game/common/KafkaTopicConfig.java
@@ -23,7 +23,7 @@ public class KafkaTopicConfig {
     @Bean
     public KafkaAdmin kafkaAdmin() {
         Map<String, Object> configs = new HashMap<>();
-        bootstrapAddress = bootstrapAddress==null? "localhost:9092" : bootstrapAddress;
+        bootstrapAddress = bootstrapAddress==null? "localhost:9093" : bootstrapAddress;
         configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         return new KafkaAdmin(configs);
     }
