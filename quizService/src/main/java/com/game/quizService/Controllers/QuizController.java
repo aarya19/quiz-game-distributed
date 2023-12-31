7741945b9be90e338452ccaf9394c16d14e96b5a
Aarya Prasannan <aaryaprasannan@gmail.com>
2023-12-31 23:51:56 +0000

    connecting start quiz and create room events

--- quizService/src/main/java/com/game/quizService/Controllers/QuizController.java
+++ quizService/src/main/java/com/game/quizService/Controllers/QuizController.java
@@ -3,10 +3,15 @@ package com.game.quizService.Controllers;
 
 import com.game.entities.Quiz;
 import com.game.entities.QuizMaster;
+import com.game.entities.Room;
 import com.game.utilities.MongoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -47,10 +52,33 @@ public class QuizController {
     }
 
 
-    @GetMapping("/startQuiz/{quizId}")
-    public ResponseEntity<String> startQuiz(@PathVariable String quizId){
-        //Fetch the quiz questions
-        //send the quiz object to the active-quizes queue
-        return ResponseEntity.ok("Started quiz with" + quizId);
+    @GetMapping("/startQuiz/{quizId}/{quizMasterUserName}")
+    public ResponseEntity<String> startQuiz(@PathVariable String quizId, @PathVariable String quizMasterUserName){
+        //Find the quiz master
+        //If the Quiz Master is not found return a Response with an error saying the quiz master does not exist
+        //If the Quiz Master exists get the quiz master and create a room with quiz Id and quiz master
+        //Send the Room as the body of the request to the next endpoint.
+
+        RestTemplate restTemplate = new RestTemplate();
+        String apiUrl = "http://localhost:8081/quizroom/create";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        List<?> quizMasterList = mongoService.findQuizMaster("_id",quizMasterUserName);
+        if(quizMasterList.isEmpty()){
+            String response = restTemplate.postForObject(apiUrl, null, String.class);
+            // Process the response as needed
+            System.out.println("Response: " + response);
+            return ResponseEntity.ok("The quiz master does not exist. The response is: "+response);
+        }
+        else{
+            QuizMaster quizMaster = (QuizMaster) quizMasterList.get(0);
+            Room room = new Room(quizId,quizMaster);
+            String response = restTemplate.postForObject(apiUrl, room, String.class);
+            System.out.println("Response: " + response);
+            return ResponseEntity.ok("Started quiz with" + quizId);
+        }
+
     }
 }
