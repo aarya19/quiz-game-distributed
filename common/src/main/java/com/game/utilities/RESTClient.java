c3acbe8399ab2617ae0698f9892f38c04b6bd08b
Rahul Raj <118106534+rajanrahulraj@users.noreply.github.com>
2024-01-02 19:48:44 +0000

    user signup and code refactor

---
+++ common/src/main/java/com/game/utilities/RESTClient.java
@@ -0,0 +1,39 @@
+package com.game.utilities;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class RESTClient {
+
+    public static ResponseEntity<String> postMessage(String url, Object data){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+
+        // Assuming Answer class has proper toString or use Jackson ObjectMapper to convert to JSON
+        String requestBody = null;
+        try {
+            requestBody = new ObjectMapper().writeValueAsString(data);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Perform the POST request
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                requestEntity,
+                String.class
+        );
+        return responseEntity;
+    }
+}
--- masterService/src/main/java/com/game/masterService/controller/MasterController.java
+++ masterService/src/main/java/com/game/masterService/controller/MasterController.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.game.common.KafkaProducerConfig;
 import com.game.entities.Answer;
+import com.game.entities.QuizMaster;
 import com.game.utilities.MongoService;
+import com.game.utilities.RESTClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -20,52 +22,41 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
-@RequestMapping("/main")
+@RequestMapping("/app")
 public class MasterController {
 
-    @Autowired
-    public MasterController() {
-
-    }
 
     @Value("${addscore.endpoint.url}")
     private String addScoreEndpoint;
+    @Value("${quiz.createQuizMaster.url}")
+    private String createUserEndpoint;
+    @Value("${game.hostUrl.quizService}")
+    private String URL_QUIZ_SERVICE;
+    @Value("${game.hostUrl.roomService}")
+    private String URL_ROOM_SERVICE;
+    @Value("${game.hostUrl.gameService}")
+    private String URL_GAME_SERVICE;
+    @Value("${game.hostUrl.scoreService}")
+    private String URL_SCORE_SERVICE;
+
     @PostMapping("/addScore")
-    public RedirectView addScore(@RequestBody Answer playerAnswer){
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-
-        // Assuming Answer class has proper toString or use Jackson ObjectMapper to convert to JSON
-        String requestBody = null;
-        try {
-            requestBody = new ObjectMapper().writeValueAsString(playerAnswer);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-
-
-        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        // Perform the POST request
-        ResponseEntity<String> responseEntity = restTemplate.exchange(
-                addScoreEndpoint,
-                HttpMethod.POST,
-                requestEntity,
-                String.class
-        );
-
+    public void addScore(@RequestBody Answer playerAnswer){
+        ResponseEntity<String> responseEntity = RESTClient.postMessage(URL_SCORE_SERVICE + addScoreEndpoint, playerAnswer);
         // Check the response if needed
         System.out.println("Response from external service: " + responseEntity.getBody());
 
-        RedirectView redirectView = new RedirectView();
-        // Set the URL to redirect to after the POST request
-        redirectView.setUrl("https://your-redirect-url");
-
-        return redirectView;
-
+//        RedirectView redirectView = new RedirectView();
+//        // Set the URL to redirect to after the POST request
+//        redirectView.setUrl("https://your-redirect-url");
+//        return redirectView;
     }
+
+    @PostMapping("signup")
+    public String createUser(@RequestBody QuizMaster quizMaster){
+        ResponseEntity<String> responseEntity = RESTClient.postMessage(URL_QUIZ_SERVICE+createUserEndpoint, quizMaster);
+        return responseEntity.getBody();
+    }
+
+
 
 }
