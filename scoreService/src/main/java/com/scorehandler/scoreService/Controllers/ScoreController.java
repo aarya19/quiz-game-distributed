7273b60dc7d58c6a706cf0d1d6247d172739d405
Aarya Prasannan <aaryaprasannan@gmail.com>
2024-01-03 13:54:32 +0000

    DB update for player scores and API endpoint for quiz end

--- scoreService/src/main/java/com/scorehandler/scoreService/Controllers/ScoreController.java
+++ scoreService/src/main/java/com/scorehandler/scoreService/Controllers/ScoreController.java
@@ -1,15 +1,26 @@
 package com.scorehandler.scoreService.Controllers;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.game.common.KafkaProducerConfig;
 import com.game.core.Constants;
 import com.game.entities.Answer;
+import com.game.entities.Player;
 import com.game.entities.ScoreEvent;
 import com.game.utilities.MongoService;
 import com.scorehandler.scoreService.service.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+
+import java.util.List;
 
 import static com.game.core.Constants.SCORE_EVENTS;
 
@@ -22,6 +33,8 @@ public class ScoreController {
     private KafkaProducerConfig producer;
 
     private ScoreService scoreService;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
 
     @Autowired
@@ -49,31 +62,37 @@ public class ScoreController {
     }
 
 
-    @GetMapping("/getFinalScores")
-    public String addScore(@RequestParam String quizId){
+    @GetMapping("/endQuiz/{quizId}")
+    public ResponseEntity<String> getAllScores(@PathVariable String quizId) {
         String res = "Getting the final player scores";
+        //fetch all the players of in the quiz from the DB.
+        List<Player> playerList = mongoService.getAllPlayers(quizId);
 
-        //fetch all the players and scores from the database
-        //order the player according to the score
-        //send back the json to the webHook
+        //sort them according to scores.
+        Comparator<Player> playerComparator = Comparator.comparingInt(Player::getScore);
+        Collections.sort(playerList,playerComparator);
 
-        return res;
+        // Convert the list of players to JSON
+        try {
+            String playerListJson = objectMapper.writeValueAsString(playerList);
+
+            // Include the JSON in the ResponseEntity
+            return new ResponseEntity<>(playerListJson, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error converting player list to JSON", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
     //add a listener to listen to scores and update in the DB.
-
     @KafkaListener(topics = SCORE_EVENTS, groupId = "score_events")
     public void persistScore(ScoreEvent event){
         if (event.getEventType().equals(Constants.EVENT_TYPE.UPDATE_SCORE.event())){
+
             System.out.println("We have recieved the answer for a player");
             System.out.println("The player id is:" + event.getPlayerId());
+            mongoService.updateScore(event.getPlayerId());
             System.out.println("The score update is, as always 1:");
-
-        }
-        if (event.getEventType().equals(Constants.EVENT_TYPE.END_QUIZ.event())){
-
-            //send all the player scores to webhooks
         }
     }
 
