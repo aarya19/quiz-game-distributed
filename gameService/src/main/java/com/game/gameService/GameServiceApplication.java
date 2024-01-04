65e633173b84cb215c73ccbe9cf62c6edcac1915
Tehsein Firoze <73216889+tfiroze@users.noreply.github.com>
2024-01-04 16:22:34 +0000

    containerization

--- gameService/src/main/java/com/game/gameService/GameServiceApplication.java
+++ gameService/src/main/java/com/game/gameService/GameServiceApplication.java
@@ -1,4 +1,4 @@
-package com.game.gameService;
+package com.game.gameservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
